9629aefe8cfc5a0100746e36036e6aadecc1f2ec
mustafa60x <mturkoz93@gmail.com>
2025-02-13 15:44:30 +0300

    server component example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
 
-import {useState, useEffect} from "react";
 
-export default function Home() {
-  const [data, setData] = useState<{ title: string }[]>([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(data => setData(data));
-    }
-
-    fetchData();
-  }, []);
+export default async function Home() {
+  const dataFetch = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await dataFetch.json() as { title: string }[];
 
   return (
     <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
