5a0c285046b4f47d280e98477f2f9ddccb53d1f4
mustafa60x <mturkoz93@gmail.com>
2025-02-13 15:42:30 +0300

    use client example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import {useState, useEffect} from "react";
+
 export default function Home() {
+  const [data, setData] = useState<{ title: string }[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(data => setData(data));
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div>
-        Hello, World!
+        Merhaba
+        {data.map((item, index) => (
+          <p key={index}>{item.title}</p>
+        ))}
       </div>
     </div>
   );
