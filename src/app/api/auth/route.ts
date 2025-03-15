ee5c477e674f90d7cd7af2e713a7de6abb1d6d90
mustafa60x <mturkoz93@gmail.com>
2025-03-15 08:29:39 +0300

    Add fake response for Eslint warn

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,1 +1,17 @@
 // api/auth
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  console.log(request);
+  return NextResponse.json({ message: "Hello from auth endpoint" });
+}
+
+export async function POST(request: Request) {
+  console.log(request);
+  return NextResponse.json({ message: "Hello from auth endpoint" });
+}
+
+export async function PUT(request: Request) {
+  console.log(request);
+  return NextResponse.json({ message: "Hello from auth endpoint" });
+}
